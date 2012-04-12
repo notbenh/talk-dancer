116ce9a94f1a4dfeaf893dbeed4c4e9656a4e295
ben hengst <ben.hengst@gmail.com>
2012-04-11 23:18:05 -0700

    getting back in the swing of things

--- lib/Talk.pm
+++ lib/Talk.pm
@@ -1,10 +1,19 @@
 package Talk;
 use Dancer ':syntax';
+use YAML qw{LoadFile};
+use FindBin qw($Bin);
 
 our $VERSION = '0.1';
+our $slides  = LoadFile(qq{$Bin/slides.yaml});
 
 get '/' => sub {
     template 'index';
 };
+
+get '/talk' => sub{ forward q{/talk/0} };
+get '/talk/:page' => sub{ 
+  template talk => {slide => $slides->[params->{page}] };
+};
+  
 
 true;
